723fbe9b3e8bf669b5dd4a3bfedaf64f72dce7c9
solingoran99 <solin.goran.yusuf.rasid@iths.se>
2024-09-03 16:02:13 +0200

    completed lab

--- NumbersLab1/Program.cs
+++ NumbersLab1/Program.cs
@@ -7,29 +7,63 @@ namespace NumbersLab1
 		{
 
 			string myNumbers = "81575187k62387623593465387469";
+			long totalSum = 0;
 
 			while (true)
 			{
-				// Ask the user to type a number from 1-9 to find matches in my string & give the option to exit
-
+				
 				Console.ForegroundColor = ConsoleColor.Blue;
 				Console.WriteLine("To find matches enter a number from 1-9.\n(Type 'exit' to leave the applicaton)");
+				Console.ResetColor();
 				string userInput = Console.ReadLine();
 
-				//break the loop if 'exit'
+				
 				if (userInput.ToLower().Trim() == "exit")
 				{
-                    Console.WriteLine("GOODBYE");
+					Console.ForegroundColor = ConsoleColor.Red;
+				    Console.WriteLine("GOODBYE");
+					Console.ResetColor();
 					break;
                 }
-				int userNumber = 0;
 
-				else
+				string regex = userInput;
+				Match match1 = Regex.Match(myNumbers, regex);
+
+				if (match1.Success)
 				{
+					int startMatch = match1.Index;
 
+					Match match2 = Regex.Match(myNumbers.Substring(startMatch + 1), regex);
+
+					if (match2.Success)
+					{
+
+						int endMatch = startMatch + match2.Index + match2.Length + 1;
+						string matchedString = myNumbers.Substring(startMatch, endMatch - startMatch);
+
+						totalSum += long.Parse(matchedString);
+
+						Console.Write(myNumbers.Substring(0, startMatch));
+						Console.ForegroundColor = ConsoleColor.Magenta;
+						Console.Write(myNumbers.Substring(startMatch, endMatch - startMatch));
+						Console.ResetColor();
+						Console.WriteLine(myNumbers.Substring(endMatch));
+						totalSum += long.Parse(matchedString);
+
+					}
+					else
+					{
+						Console.WriteLine(myNumbers);
+					}
 				}
+				else 
+				{
+                    Console.WriteLine(myNumbers);
+                }
+				Console.ReadLine();
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine($"Totalt = {totalSum}");
 
-			
 
 			}
 			
